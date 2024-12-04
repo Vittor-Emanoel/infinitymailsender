07e771cf048cdc508bc74e6b66a690b1735baa9b
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2024-12-03 23:46:52 -0300

    adicionado os metodos findById() | findByEmail() | listAll()

--- src/interfaces/contact-interface.ts
+++ src/interfaces/contact-interface.ts
@@ -1,15 +1,20 @@
-type Contact = {
+export type Contact = {
     id: string
     identifier: string
     email: string
     subscribed: boolean
 }
 
-interface IContactRepository {
-    createMany: (contacts: Omit<Contact, 'id'>[]) => Promise<void>
+export interface IContactRepository {
+    createMany: (
+        contacts: Omit<Contact, 'id' | 'subscribed'>[],
+    ) => Promise<void>
     changeSubscription: (
         contactId: string,
         subscribed: boolean,
     ) => Promise<void>
+    findById: (contactId: string) => Promise<Contact | null>
+    findByEmail: (contactEmail: string) => Promise<Contact | null>
+    listAll: () => Promise<Contact[]>
     delete: (contactId: string) => Promise<void>
 }
