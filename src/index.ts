d4d55b36546240b3793f5c44ef02661a4596e07a
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-12-03 22:48:01 -0300

    feat: create route import contacts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { env } from "env/env";
+import multipart from "@fastify/multipart";
 import { fastify } from "fastify";
+import { appRoutes } from "routes";
+import { env } from "./env";
 
 const app = fastify();
+app.register(multipart);
+app.register(appRoutes);
 
 app.listen(
   {
